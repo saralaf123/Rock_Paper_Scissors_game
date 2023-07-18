271e04c1240d1efe994aeb78203eb5ebf14e2c64
saralaf123 <saralafeta@gmail.com>
2023-07-18 11:57:46 +0100

    Initial commit

---
+++ src/main/java/RockPaperScissorsgame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RockPaperScissorsgame {
+
+    public static void main(String[] args) {
+        String rock="Rock";
+        String paper="Paper";
+        String scissor="Scissor";
+        String usersName1="khalid";
+        String userName2="Sara";
+        Scanner scanner=new Scanner(System.in) ;
+
+    }
+}
